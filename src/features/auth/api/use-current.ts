2052494d5108804b2312d8a7a8b78ec07d5709e6
DrakeGoCoding <hoanglongphi21@gmail.com>
2024-10-23 23:47:26 +0700

    session middleware

---
+++ src/features/auth/api/use-current.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { client } from '@/lib/rpc';
+
+export const useCurrent = () => {
+  const query = useQuery({
+    queryKey: ['current'],
+    queryFn: async () => {
+      const res = await client.api.auth.current.$get();
+
+      if (!res.ok) {
+        return null;
+      }
+
+      const { user } = await res.json();
+
+      return user;
+    }
+  });
+
+  return query;
+};
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -4,11 +4,17 @@ import { deleteCookie, setCookie } from 'hono/cookie';
 import { ID } from 'node-appwrite';
 
 import { createAdminClient } from '@/lib/appwrite';
+import { sessionMiddleware } from '@/lib/session-middleware';
 
 import { AUTH_COOKIE } from '../constants';
 import { loginSchema, registerSchema } from '../schema';
 
 const app = new Hono()
+  .get('/current', sessionMiddleware, async (c) => {
+    const user = c.get('user');
+
+    return c.json({ user });
+  })
   .post('/login', zValidator('json', loginSchema), async (c) => {
     const { email, password } = c.req.valid('json');
 
@@ -43,8 +49,11 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post('/logout', async (c) => {
+  .post('/logout', sessionMiddleware, async (c) => {
+    const account = c.get('account');
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession('current');
 
     return c.json({ success: true });
   });
