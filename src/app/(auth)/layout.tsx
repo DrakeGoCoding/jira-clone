4c578d62d1d5d9037932e3211c722aa9e408d8af
DrakeGoCoding <hoanglongphi21@gmail.com>
2024-10-24 21:06:55 +0700

    refactor actions->queries

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 import { AuthNavbar } from '@/components/auth-navbar';
-import { getCurrent } from '@/features/auth/actions';
+import { getCurrent } from '@/features/auth/queries';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
